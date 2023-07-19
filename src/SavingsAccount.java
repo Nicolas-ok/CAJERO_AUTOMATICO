cf5618dde3912f64570c7045dbc66dd0283bc1b0
Nicolas Fernandez <smandes@gmail.com>
2023-07-18 23:04:35 -0300

    Security update 0.2

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class CheckingAccount extends BankAccount {
+public class SavingsAccount extends BankAccount {
 
-	private double overdraftLimit;
+	private final int TIME_PERIOD = 3;
+	private final double MIN_ACTUAL_BALANCE = 0.5;
+	private final double INTEREST_RATE = 0.3;
 	private List<Transaction> transactions;
+	private double minimumBalance;
+	Scanner scanner = new Scanner(System.in, "UTF-8");
 
-	public CheckingAccount(String accountNumber, double balance, double overdraftLimit) {
+	public SavingsAccount(String accountNumber, double balance) {
 		super(accountNumber, balance);
-		setOverdraftLimit(overdraftLimit);
 		setTransactions();
 	}
 
@@ -16,9 +20,13 @@ public class CheckingAccount extends BankAccount {
 		this.transactions = new ArrayList<>();
 	}
 
+	public double getINTEREST_RATE() {
+		return INTEREST_RATE;
+	}
+
 	@Override
 	public void withdraw(double amount) {
-		if (balance - amount < -overdraftLimit) {
+		if (amount > balance) {
 			throw new IllegalArgumentException("No hay suficientes fondos disponibles para realizar esta operación");
 		}
 		balance -= amount;
@@ -30,49 +38,68 @@ public class CheckingAccount extends BankAccount {
 			throw new IllegalArgumentException("El monto a depositar debe ser mayor a 0.");
 		}
 		balance += amount;
+		applyInterest();
 		transactions.add(new Transaction("Depósito", amount));
 	}
 
-	public void transfer(double amount, CheckingAccount destinationAccount) {
-		if (amount <= 0) {
-			throw new IllegalArgumentException("El monto a transferir debe ser mayor a 0.");
-		}
-		if (destinationAccount == null) {
-			throw new IllegalArgumentException("La cuenta destino es inválida o no existe.");
-		}
-		if (balance < amount) {
-			throw new IllegalStateException("No tienes saldo suficiente para realizar esta transferencia.");
-		}
-		balance -= amount;
-		destinationAccount.balance += amount;
-		transactions.add(new Transaction("Transferencia", -amount));
+	public void applyInterest() {
+		double interest = balance * INTEREST_RATE;
+		System.out.println("Interés generado: $" + interest);
+	}
+
+	public double accumulatedInterest() {
+		double accumulatedInterest = balance * INTEREST_RATE * TIME_PERIOD;
+		return accumulatedInterest;
 	}
 
 	public void checkBalance() {
-		System.out.println("El saldo actual de tu cuenta es: $" + balance);
-		if (balance < 0) {
-			System.out.println("ATENCIÓN: Tu cuenta se encuentra en sobregiro.");
+		System.out.println("Saldo actual: $" + balance);
+		System.out.println("Saldo mínimo: $" + minimumBalance);
+		System.out.println("Saldo disponible: $" + (balance - minimumBalance));
+		System.out.println("Interés acumulado: $" + accumulatedInterest());
+		System.out.println("\nHistorial de transacciones: ");
+		for (Transaction transaction : transactions) {
+			System.out.println(transaction.getDescription() + " - $" + transaction.getAmount() + " - "
+					+ transaction.getDateTime());
 		}
 	}
 
-	public void setOverdraftLimit(double overdraftLimit) {
-		if (overdraftLimit < 0) {
-			throw new IllegalArgumentException("El límite de sobregiro debe ser mayor o igual a 0.");
+	protected void setMinimumBalance(double minimumBalance) {
+		if (minimumBalance < 0) {
+			throw new IllegalArgumentException("El saldo mínimo debe ser mayor o igual a 0.");
 		}
-		if (overdraftLimit > balance) {
-			throw new IllegalArgumentException("El límite de sobregiro no puede ser mayor al saldo actual de la cuenta.");
+		if (minimumBalance > (balance * MIN_ACTUAL_BALANCE)) {
+			throw new IllegalArgumentException(
+					"El saldo mínimo no puede ser mayor al 50% del saldo actual de la cuenta.");
 		}
-		// Establecer un límite máximo para el límite de sobregiro
-		double maxOverdraftLimit = balance * 0.5;
-		this.overdraftLimit = Math.min(overdraftLimit, maxOverdraftLimit);
+		if (minimumBalance % 1 != 0) {
+			throw new IllegalArgumentException("El saldo mínimo debe ser un valor entero.");
+		}
+		this.minimumBalance = minimumBalance;
 	}
 
-	public double getOverdraftLimit() {
-		return overdraftLimit;
+	public void transfer(double amount, BankAccount destination) {
+		if (amount <= 0) {
+			throw new IllegalArgumentException("El monto a transferir debe ser mayor a 0.");
+		}
+		if (destination == null || destination.equals(this)) {
+			throw new IllegalArgumentException("La cuenta de destino no es válida.");
+		}
+		if (balance - amount < minimumBalance) {
+			throw new IllegalStateException("No tienes saldo suficiente para realizar la transferencia.");
+		}
+		balance -= amount;
+		destination.deposit(amount);
+		transactions.add(new Transaction("Transferencia", amount));
 	}
 
 	@Override
 	public String getType() {
-		return "Cuenta corriente";
+		return "Cuenta de ahorros";
 	}
+
+	public double getMinimumBalance() {
+		return minimumBalance;
+	}
+
 }
